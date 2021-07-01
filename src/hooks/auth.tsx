0011cafb46cef17965f2643eac8efd5ed524eea3
Drarkors <rafaelfzanetti@hotmail.com>
2021-07-01 20:23:47 -0300

    Log

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -110,6 +110,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
     api.defaults.headers.authorization = token;
 
+    console.log(`Api URL: ${process.env.NEXT_PUBLIC_API_URL}`)
+
     await api.get('/account/detail').then(response => {
       const isActive = user.isActive
 
