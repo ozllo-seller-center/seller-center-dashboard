75bb7411a4eb3d083356a5f917316107fb6c345c
Rafael Figueiredo Zanetti <rafaelfzanetti@hotmail.com>
2022-02-23 22:36:23 -0300

    Desativando Integracoes e adicionando complemento nos detalhes do pedido

---
+++ src/components/Textfield/index.tsx
@@ -0,0 +1,19 @@
+import { TextField, TextFieldProps } from '@material-ui/core'
+import { useField } from '@unform/core'
+import React, { useRef } from 'react'
+import Input, { InputProps } from 'react-select/src/components/Input'
+
+type CustomTextfieldProps = TextFieldProps & {
+  name: string,
+}
+
+const Textfield: React.FC<CustomTextfieldProps> = ({ name, ...rest }) => {
+
+  const inputRef = useRef<InputProps>(null)
+
+  const { fieldName, defaultValue, error, registerField } = useField(name)
+
+  return (<TextField name={name} inputRef={inputRef} {...rest} />)
+}
+
+export default Textfield
