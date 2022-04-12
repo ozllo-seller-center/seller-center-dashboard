01ab64f9a9b09063f1b7c72bd34d8cad1defcd04
Rafael Figueiredo Zanetti <rafaelfzanetti@hotmail.com>
2022-04-11 21:06:48 -0300

    Status de atrasos de entrega

--- src/components/OrderStatusPanel/index.tsx
+++ src/components/OrderStatusPanel/index.tsx
@@ -5,20 +5,34 @@ import styles from './styles.module.scss';
 type PanelProps = HTMLAttributes<HTMLDivElement> & {
   title: string;
   isActive?: boolean;
+  altAlign?: boolean;
 };
 
 const OrderStatusPanel: React.FC<PanelProps> = ({
   children,
   isActive,
+  altAlign,
   title,
   onClick,
+  ...rest
 }) => (
   <div
     onClick={onClick}
     className={isActive ? styles.containerActive : styles.container}
+    {...rest}
   >
-    <span>{title}</span>
-    <div className={styles.content}>{children}</div>
+    <span
+      className={styles.title}
+      style={altAlign ? { marginLeft: 'auto' } : {}}
+    >
+      {title}
+    </span>
+    <div
+      className={styles.content}
+      style={altAlign ? { marginLeft: 'auto' } : {}}
+    >
+      {children}
+    </div>
     {/* <div className={styles.link} onClick={onClick}>
         <a>Ver mais</a>
         <FiChevronRight />
