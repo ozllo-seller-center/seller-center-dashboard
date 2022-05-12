22a23298bc6bda50e93e61033dc5bd72bc37dc62
Daniel Hernandes <badah@badah.com.br>
2022-05-12 18:36:38 -0300

    feat: send invoice xml

--- src/components/NfeModalContent/index.tsx
+++ src/components/NfeModalContent/index.tsx
@@ -71,6 +71,8 @@ const NfeModalContent: React.FC<NfeModalContentProps> = ({
 
       // dataContainer.append("xml", blob, nfeFile.name)
 
+      const xml = await nfeFile?.text().then(data => data)
+
       const schema = Yup.object().shape({
         key: Yup.string()
           .required('Campo obrigatório')
@@ -92,6 +94,7 @@ const NfeModalContent: React.FC<NfeModalContentProps> = ({
           issuedDate: !nfeData
             ? isoDateHub2b(new Date().toISOString())
             : nfeData.issueDate,
+          ...(xml ? {xml} : {}),
         };
 
         api
