1de68f192e2cd0c0f736852e4423fea3d3bdc87e
Daniel Hernandes <badah@badah.com.br>
2022-06-22 10:56:22 -0300

    chore: remove dead code

--- src/pages/home_old.tsx
+++
@@ -1,9 +0,0 @@
-import { GetStaticProps } from 'next';
-
-const Home = () => <h1>Hello World</h1>;
-
-export const getStaticProps: GetStaticProps = async () => ({
-  props: {},
-});
-
-export default Home;
